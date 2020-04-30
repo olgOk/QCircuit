633e66a8cfc8263ad92f0cf5d6d15d75ebec882c
user <user@macRoot.local>
2020-04-30 10:20:00 -0700

    added release version

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 setup(
   name = 'qcircuit',         # How you named your package folder (MyLib)
   packages = ['qcircuit'],   # Chose the same as "name"
-  version = '1',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='Apache',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Quantum Circuit Simulator',   # Give a short description about your library
   author = 'Olga Okrut',                   # Type in your name
   author_email = 'vokrut42sv@gmail.com.com',      # Type in your E-Mail
   url = 'https://github.com/olgOk/QCircuit',   # Provide either the link to your github or to your website
   python_requires = ('>=3.6.0'),
+  download_url = "https://github.com/olgOk/QCircuit/archive/1.0.tar.gz"
 )
